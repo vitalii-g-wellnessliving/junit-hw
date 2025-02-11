3a578a607c9af10ddfc1470f7dcbc98c01459f91
Vitalii Goncharov <1c.audit@gmail.com>
2025-02-11 20:17:00 +0200

    add checkstyle

--- src/main/java/xyz/gvital/Main.java
+++ src/main/java/xyz/gvital/Main.java
@@ -1,5 +1,6 @@
 package xyz.gvital;
 
+@SuppressWarnings("hideutilityclassconstructor")
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
